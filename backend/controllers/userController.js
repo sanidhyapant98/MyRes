6e99c11b5b6f023d1ee8611fb6b6178441491ac0
Sanidhya Pant <152873379+sanidhyapant98@users.noreply.github.com>
2026-02-04 22:41:35 +0530

    feat: update userController to return user data and handle user not found error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,12 @@
+const { User } = require("../model/userModel")
+
 const userController = async (req, res)=>{
     try{
+        const user = req.user
+        if(!user){
+            res.status(500).send("User not found")
+        }
+        res.send(user)
         res.send("User Data")
     }catch(err){
         res.status(500).send("Error : ", err.message)
