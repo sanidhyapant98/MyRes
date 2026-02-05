2cb8e9658d6ff4391b7c8ab5966c5d1392a64eff
Sanidhya Pant <152873379+sanidhyapant98@users.noreply.github.com>
2026-02-05 16:22:58 +0530

    feat: add updatePassword controller and route for user password updates

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,15 @@
 const { User } = require("../model/userModel")
+const bcrypt = require('bcrypt')
 
 const userController = async (req, res)=>{
     try{
         const user = req.user
         if(!user){
-            res.status(500).send("User not found")
+            return res.status(500).send("User not found")
         }
         res.send(user)
     }catch(err){
-        res.status(500).send("Error: " + err.message)
+        return res.status(500).send("Error: " + err.message)
     }
 }
 
@@ -16,7 +17,7 @@ const updateController = async (req, res)=>{
     try{
         const user = req.user
         if(!user){
-            res.status(404).send("User not found")
+            return res.status(404).send("User not found")
         }
         const {userName, phone, address} = req.body
         if(userName){
@@ -31,11 +32,36 @@ const updateController = async (req, res)=>{
         await user.save()
         res.status(200).send("User updated successfully")
     }catch(err){
-        res.status(500).send("Error: " + err.message)
+        return res.status(500).send("Error: " + err.message)
+    }
+}
+
+const updatePassword = async (req, res)=>{
+    try{
+        const {oldPass, newPass} = req.body
+        if(!oldPass || !newPass){
+            return res.status(400).send("Both passwords required")
+        }
+        const user = req.user
+        if(!user){
+            return res.status(404).send("User not found")
+        }
+        const isMatch = await bcrypt.compare(oldPass, user.password)
+        if(!isMatch){
+            return res.status(400).send("Password does not match")
+        }
+        const salt = await bcrypt.genSaltSync(10)
+        const hashedPass = await bcrypt.hash(newPass, salt)
+        user.password = hashedPass
+        await user.save()
+        res.status(200).send("Password updated successfully")
+    }catch(err){
+        return res.status(500).send("Error: " + err.message)
     }
 }
 
 module.exports = {
     userController,
-    updateController
+    updateController,
+    updatePassword
 }
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { userController, updateController } = require('../controllers/userController')
+const { userController, updateController, updatePassword } = require('../controllers/userController')
 const { authMiddleware } = require('../middlewares/authMiddleware')
 const userRouter = express.Router()
 
 userRouter.get('/getUser', authMiddleware, userController)
 userRouter.patch('/updateUser', authMiddleware, updateController)
+userRouter.patch('/updatePassword', authMiddleware, updatePassword)
 
 module.exports = {
     userRouter
